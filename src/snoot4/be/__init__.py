b38c17ae81e179e4fa05ed1d9a830d6c315ed745
the6p4c <me@doggirl.gay>
2023-12-06 17:59:03 +1000

    add bypass paths in backend

--- src/snoot4/be/__init__.py
+++ src/snoot4/be/__init__.py
@@ -17,7 +17,9 @@ class Backend(Component):
         m.submodules.arith = arith = Arith()
 
         # === pipeline registers ===
+        dx_ra = Signal(4)
         dx_ra_val = Signal(32)
+        dx_rb = Signal(4)
         dx_rb_val = Signal(32)
         dx_r0_val = Signal(32)
         dx_imm = Signal(32)
@@ -46,6 +48,8 @@ class Backend(Component):
             dx_r0_val.eq(rf.r0),
         ]
         m.d.sync += [
+            dx_ra.eq(decoder.ra),
+            dx_rb.eq(decoder.rb),
             dx_imm.eq(decoder.imm),
             dx_op2_sel.eq(decoder.op2_sel),
             dx_rd_en.eq(decoder.rd_en),
@@ -53,14 +57,33 @@ class Backend(Component):
         ]
 
         # === X ===
+        def _bypass(*, rn, rn_val):
+            byp_val = Signal(32)
+            with m.If(xm_rd_en & (xm_rd == rn)):
+                m.d.comb += byp_val.eq(xm_rd_val)
+            with m.Elif(mw_rd_en & (mw_rd == rn)):
+                m.d.comb += byp_val.eq(mw_rd_val)
+            with m.Else():
+                m.d.comb += byp_val.eq(rn_val)
+            return byp_val
+
+        x_ra_val = Signal(32)
+        x_rb_val = Signal(32)
+        x_r0_val = Signal(32)
+        m.d.comb += [
+            x_ra_val.eq(_bypass(rn=dx_ra, rn_val=dx_ra_val)),
+            x_rb_val.eq(_bypass(rn=dx_rb, rn_val=dx_rb_val)),
+            x_r0_val.eq(_bypass(rn=0, rn_val=dx_r0_val)),
+        ]
+
         x_op1 = Signal(32)
         x_op2 = Signal(32)
-        m.d.comb += x_op1.eq(dx_ra_val)
+        m.d.comb += x_op1.eq(x_ra_val)
         with m.Switch(dx_op2_sel):
             with m.Case(Op2Sel.RB):
-                m.d.comb += x_op2.eq(dx_rb_val)
+                m.d.comb += x_op2.eq(x_rb_val)
             with m.Case(Op2Sel.R0):
-                m.d.comb += x_op2.eq(dx_r0_val)
+                m.d.comb += x_op2.eq(x_r0_val)
             with m.Case(Op2Sel.IMM):
                 m.d.comb += x_op2.eq(dx_imm)
 
@@ -106,8 +129,12 @@ if __name__ == "__main__":
         yield uut.instruction.eq(0b0000_0000_0000_1001)
         yield
         # ADD #imm,Rn   Rn + imm -> Rn    0111 nnnn iiii iiii
-        # ADD #1,R4     R4 + 1   -> R4    0111 0100 0000 0001
-        yield uut.instruction.eq(0b0111_0100_0000_0001)
+        # ADD #1,R2     R2 + 1   -> R2    0111 0100 0000 0001
+        yield uut.instruction.eq(0b0111_0010_0000_0001)
+        yield
+        # ADD #imm,Rn   Rn + imm -> Rn    0111 nnnn iiii iiii
+        # ADD #3,R4     R4 + 3   -> R4    0111 0100 0000 0011
+        yield uut.instruction.eq(0b0111_0100_0000_0011)
         yield
         # ADD Rm,Rn   Rn + Rm -> Rn    0011 nnnn mmmm 1100
         # ADD R2,R4   R4 + R2 -> R4    0011 0100 0010 1100
