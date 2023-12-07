a2ba091b8fd1a196ae3d50b915397e2b942528d3
the6p4c <me@doggirl.gay>
2023-12-07 14:11:59 +1000

    show instruction name in vcd

--- src/snoot4/be/decoder.py
+++ src/snoot4/be/decoder.py
@@ -2,7 +2,7 @@ from contextlib import contextmanager
 import re
 from types import SimpleNamespace
 
-from amaranth import Cat, Module
+from amaranth import Cat, Module, Signal
 from amaranth.lib import enum
 from amaranth.lib.wiring import Component, In, Out
 
@@ -44,9 +44,20 @@ class Decoder(Component):
     def elaborate(self, platform):
         m = Module()
 
+        # store instruction names to provide a slightly nicer output in the VCD
+        #
+        # although the mapping is dependendent on the order in the file, this mapping is entirely
+        # internal so it's unlikely to be an issue
+        asm_names = []
+        asm = Signal(32, decoder=lambda index: asm_names[index])
+
         @contextmanager
-        def _instruction(pattern):
+        def _instruction(pattern, name):
             WILDCARDS = "mnid"
+
+            # === store instruction ===
+            asm_index = len(asm_names)
+            asm_names.append(name)
 
             # === extract fields ===
             # reversed with spaces removed so that string indices match bit indices
@@ -72,7 +83,10 @@ class Decoder(Component):
             matcher = re.sub(f"[{WILDCARDS}]", "-", pattern)
 
             with m.If(self.instruction.matches(matcher)):
-                m.d.comb += self.valid.eq(1)
+                m.d.comb += [
+                    self.valid.eq(1),
+                    asm.eq(asm_index),
+                ]
                 yield SimpleNamespace(**fields)
 
         def _rf(*, ra=None, rb=None, rd=None):
@@ -103,37 +117,35 @@ class Decoder(Component):
         def _with_rd(rd_sel):
             m.d.comb += self.rd_sel.eq(rd_sel)
 
-        # MOV #imm,Rn
-        with _instruction("1110nnnniiiiiiii") as inst:
+        # === fixed-point transfer instructions ===
+        with _instruction("1110nnnniiiiiiii", "MOV #imm,Rn") as inst:
             _rf(rd=inst.n)
             _use_simm(inst.i)
             _with_op2(Op2Sel.IMM)
             _with_rd(RdSel.OP2)
 
-        # ADD Rm, Rn
-        with _instruction("0011 nnnn mmmm 1100") as inst:
+        # === arithmetic operation instructions ===
+        with _instruction("0011 nnnn mmmm 1100", "ADD Rm,Rn") as inst:
             _rf(ra=inst.n, rb=inst.m, rd=inst.n)
             _with_op2(Op2Sel.RB)
             _use_arith(Arith.Sel.ADD)
             _with_rd(RdSel.ARITH)
 
-        # ADD #imm,Rn
-        with _instruction("0111 nnnn iiii iiii") as inst:
+        with _instruction("0111 nnnn iiii iiii", "ADD #imm,Rn") as inst:
             _rf(ra=inst.n, rd=inst.n)
             _use_simm(inst.i)
             _with_op2(Op2Sel.IMM)
             _use_arith(Arith.Sel.ADD)
             _with_rd(RdSel.ARITH)
 
-        # AND Rm,Rn
-        with _instruction("0010 nnnn mmmm 1001") as inst:
+        # === logic operation instructions ===
+        with _instruction("0010 nnnn mmmm 1001", "AND Rm,Rn") as inst:
             _rf(ra=inst.n, rb=inst.m, rd=inst.n)
             _with_op2(Op2Sel.RB)
             _use_logic(Logic.Sel.AND)
             _with_rd(RdSel.LOGIC)
 
-        # NOT Rm,Rn
-        with _instruction("0110 nnnn mmmm 0111") as inst:
+        with _instruction("0110 nnnn mmmm 0111", "NOT Rm,Rn") as inst:
             _rf(rb=inst.m, rd=inst.n)
             _with_op2(Op2Sel.RB)
             _use_logic(Logic.Sel.NOT)
